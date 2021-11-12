2cfc69554cec383cc90f6dc28ca19d67da6ce782
mrtasln <murat_1992_51@outlook.com>
2021-11-12 21:12:24 +0300

    Updates

--- src/store/actions/covidStateAction.ts
+++
@@ -1,6 +0,0 @@
-import {CovidStateDispatch} from "../../types/covidState";
-
-export const selectActiveCovidState = (state: string) => async (dispatch: CovidStateDispatch) => {
-
-    dispatch({type: "SELECT_STATE", payload: state});
-};
